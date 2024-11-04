13c32e37e40beac7d36c3f76044a51de9a0d365b
dino.raosavac@study.thws.de <dino.raosavac@study.thws.de>
2024-11-04 18:34:27 +0100

    --

---
+++ tempHumiditySensor.py
@@ -0,0 +1,26 @@
+import time
+import board
+import adafruit_dht
+
+# Initialize the DHT device, with data pin connected to:
+dhtDevice = adafruit_dht.DHT22(board.D4)
+
+while True:
+    try:
+        # Get temperature in Celsius and humidity
+        temperature_c = dhtDevice.temperature
+        humidity = dhtDevice.humidity
+
+        # Print the values in Celsius and humidity only
+        print("Temp: {:.1f} C    Humidity: {}%".format(temperature_c, humidity))
+
+    except RuntimeError as error:
+        # Errors happen fairly often with DHT sensors, just keep going
+        print(error.args[0])
+        time.sleep(2.0)
+        continue
+    except Exception as error:
+        dhtDevice.exit()
+        raise error
+
+    time.sleep(2.0)
